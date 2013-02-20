61fe525c7fcbc91e1aeb5c86f767469868efcf3e
Shenbaga Prasanna <praserocking@gmail.com>
2013-02-20 05:13:40 -0800

    Create ACPC10A.c

---
+++ ACPC10A.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int a, b, c;
+	while(scanf("%d%d%d", &a, &b, &c)==3 && (a|b|c)) {
+		if(b - a == c - b) printf("AP %d\n", c + c - b);
+		else printf("GP %d\n", c * c / b);
+	}
+	return 0;
+}
