c47780f38703a538b9adb575f01ebbd36b38ad9c
Shenbaga Prasanna <praserocking@gmail.com>
2013-03-20 22:19:05 +0530

    Update REVINPUT.php

--- REVINPUT.php
+++ REVINPUT.php
@@ -1,11 +1,11 @@
 <?php
 fscanf(STDIN,"%d",$n);
-$line=trim(fgets(STDIN));
-$piece=explode(" ",$line);
-$i=count($piece)+1;
+$l=trim(fgets(STDIN));
+$p=explode(" ",$l);
+$i=count($p)+1;
 do
 {
-$k=strrev($piece[$i]);
+$k=strrev($p[$i]);
 for($x=0;$x<$n;$x++)
 echo $k;
 $i--;
