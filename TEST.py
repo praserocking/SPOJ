b086aaf239d075339fe6403f7b9325cda9a4761d
Shenbaga Prasanna <praserocking@gmail.com>
2013-06-11 11:32:06 +0530

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,11 +1,7 @@
-import sys
+while 1:
+	t=input()
+	if t!=42:
+		print t
+	else:
+		break
  
-def main():
-  for line in sys.stdin:
-    if int(line) == 42:
-      break
-    else:
-      print int(line)
- 
-if __name__ == '__main__':
-  main() 
