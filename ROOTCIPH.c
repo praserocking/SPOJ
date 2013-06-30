4ac273c6204f267a6478b860cd11f18df92046c9
Shenbaga Prasanna <praserocking@gmail.com>
2013-06-30 20:42:14 +0530

    Create ROOTCIPH.c

---
+++ ROOTCIPH.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+/* CODER: prassi */
+/* B.Tech,IT.,SASTRA University,Tanjore. */
+int main()
+{long long int t,a,b,c;scanf("%lld",&t);while(t--){scanf("%lld%lld%lld",&a,&b,&c);printf("%lld\n",(a*a)-(2*b));}return 0;}
