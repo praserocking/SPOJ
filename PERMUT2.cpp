8f5a9e89b5c9af4d8720960b6970482940ccf85e
Shenbaga Prasanna <praserocking@gmail.com>
2013-07-09 21:24:25 +0530

    Create PERMUT2.cpp

---
+++ PERMUT2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstdio>
+#include<cmath>
+#include<ctime>
+#include<cstdlib>
+#include<queue>
+#include<cstring>
+#include<cctype>
+#define mn(a,b) a>b?b:a
+#define loop(n,i) for(int i=0;i<n;i++)
+#define mx(a,b) a>b?a:b
+#define PI 3.14159
+#define put printf
+#define get scanf
+#define all(a) a.begin(),a.end()
+#define iter vector<int>::iterator
+#define szf sizeof
+#define setfiles freopen("in.txt","r",stdin);freopen("out.txt","w",stdout);
+template <class type>type fact(type n){return n==1?1:fact(n-1)*n;}
+template <class type>type gcd(type a,type b){return b==0?a:gcd(b,a%b);}
+typedef long long int ll;
+typedef unsigned long long int ull;
+using namespace std;
+inline int fast_int()
+{
+int n=0;
+int ch=getchar_unlocked();
+while(ch<48)ch=getchar_unlocked();
+while(ch >47)
+n=(n<<3)+(n<<1)+ch-'0',ch=getchar_unlocked();
+return n;
+}
+/* CODER: prassi */
+/* B.Tech,IT.,SASTRA University,Tanjore. */
+int main(){
+    int flag;
+    while(true){
+  	int n=fast_int();flag=0;
+		if(n==0)break;
+		if(n==1){n=fast_int();put("ambiguous\n");continue;}
+		int a[n],b[n];
+		loop(n,i){
+			a[i]=fast_int();
+			b[a[i]-1]=i+1;
+		}
+		loop(n,i){
+			if(a[i]!=b[i]){
+				put("not ambiguous\n");flag=1;
+				break;
+			}
+		}
+		if(flag==1)continue;
+		else put("ambiguous\n");
+	}
+return 0;}
